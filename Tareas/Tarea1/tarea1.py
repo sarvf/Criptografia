7f4ab4166e7bc8adf011044a39d3fe7b2133d648
Lucas Peñailillo <51519155+sarvf@users.noreply.github.com>
2022-05-08 21:37:47 -0400

    Update tarea 1

--- Tareas/Tarea1/tarea1.py
+++ Tareas/Tarea1/tarea1.py
@@ -1,102 +1,132 @@
-def hexadecimalToDecimal(hexval):
-    length = len(hexval)
-    base = 1
-    dec_val = 0
-    for i in range(length - 1, -1, -1):
-        if hexval[i] >= '0' and hexval[i] <= '9':
-            dec_val += (ord(hexval[i]) - 48) * base
-            base = base * 16
-        elif hexval[i] >= 'A' and hexval[i] <= 'F':
-            dec_val += (ord(hexval[i]) - 55) * base
-            base = base * 16
-    return dec_val
-def stringHex(char):
-    if(char != 'a' and char != 'b'and char != 'c'and char != 'd'and char != 'e'and char != 'f'):
-        return int(char)
-    else:
-        if(char=='a'):
-            return 10
-        if(char=='b'):
-            return 11
-        if(char=='c'):
-            return 12
-        if(char=='d'):
-            return 13
-        if(char=='e'):
-            return 14
-        if(char=='f'):
-            return 15
+# # la realizacion de la tarease realizo en conjunto con:
+# Felipe Ulloa e Iman jarufe, por lo que es posible que 
+# los codigos se parescan bastante
+#
+#
+
+def stringHexToHex(char):
+    if(char=='a'):return 10
+    elif(char=='b'):return 11
+    elif(char=='c'):return 12
+    elif(char=='d'):return 13
+    elif(char=='e'):return 14
+    elif(char=='f'):return 15
+    else: return int(char)
+
 def ASCIItoHEX(ascii):   
-    hexa = ""
-    for i in range(len(ascii)):
-        ch = ascii[i]
-        in1 = ord(ch)
-        part = hex(in1).lstrip("0x").rstrip("L")
-        hexa += part
-    return hexa
-def hexToASCII(hexx):
-    ascii=""
-    for i in range(0, len(hexx), 2):
-        part = hexx[i : i + 2]
-        ch = chr(int(part, 16))
-        ascii += ch
-    return ascii
-def ConstruirMatriz(h,indices):
-    llave=[""]
-    llave.clear()
-    x=j=0
-    hexL=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    hexString=""
+    for i in range (len(ascii)):
+        parHex=format(ord(ascii[i]), "x")
+        hexString+=parHex[0]+parHex[1]
+    return list(hexString)
+
+def ConstruirMatriz(hexLlave,indices):
+    NorepeatHexllave=[]
+    universoMatriz=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
     matriz=[['','','',''],['','','',''],['','','',''],['','','','']]
     cnt=0
-    e=0
-    for letter in h:
-        if (llave.count(letter)==0):
-            llave.append(letter)
-            hexL.pop(hexL.index(letter))
-    for x in range(4):
-        for j in range(4):
-            if (cnt >=len(llave)):
-                # print("if")
-                matriz[x][j]= hexL.pop(0)
-                # print("1 error N ",e)
-                # e+=1
-                indices[stringHex(matriz[x][j])][0]=x
-                # print("2 error N ",e)
-                # e+=1
-                indices[stringHex(matriz[x][j])][1]=j
 
+    #Agregamos la primera vez de aparicion de un hex en la llave y lo eliminamos del universo de caracteres de la matriz del MAL  
+    for OneHex in hexLlave:   
+        if (NorepeatHexllave.count(OneHex)==0):
+            NorepeatHexllave.append(OneHex)
+            universoMatriz.pop(universoMatriz.index(OneHex))
+        
+    #Ya con los hex de la llave sin repetir y quitados del universo de posibilidades de caracteres de la matriz, se procede a llenar los primeros elementos de la matriz, con los hex sin repetir, y luego con los elementos del universo que quedaron en orden secuencial.
+    for x in range(4):#Filas
+        for j in range(4):#Columnas
+            if (cnt >=len(NorepeatHexllave)):
+                matriz[x][j]= universoMatriz.pop(0)
+                indices[stringHexToHex(matriz[x][j])]=[x,j]
             else:
-                # print("else")
-                matriz[x][j]= llave[cnt]
-                # print("3 error N ",e)
-                # e+=1
-                indices[stringHex(matriz[x][j])][0]=x
-                # print("4 error N ",e)
-                # e+=1
-                indices[stringHex(matriz[x][j])][1]=j
+                matriz[x][j]= NorepeatHexllave[cnt]
+                indices[stringHexToHex(matriz[x][j])]=[x,j]
                 cnt+=1
-    print(indices)
-    return matriz
+    return matriz 
+
+
 def desplazamiento(FilaColumna1, FilaColumna2):
     if(FilaColumna1[0] == FilaColumna2[0] and FilaColumna1[1] != FilaColumna2[1]):
-        return "Vertical"
+        return "V"
     elif(FilaColumna1[0] != FilaColumna2[0] and FilaColumna1[1] == FilaColumna2[1]):
-        return "Horizontal"
+        return "H"
     else:
-        return "Diagonal"
+        return "D" 
 
-# def CifrarMensaje(matriz,mensaje):
-#     return
+
+
+def CifrarMensaje(matriz,indices,mensaje):
+    
+    # 1.-Pasar Mensaje ASCII a Lista de String hex
+    # Ciclo por cada par de hex de la lista
+    # 2.-Para cada caracter ASCII, 2hex, Buscar los indices de cada hex (con la matriz indices)
+    # 4.-A partir de los indices buscar que operacion se debe realizar deplazamiento()
+    # 5.-Operar los indices segun la operacion, Ej: Diagonal indicesHex1=+1Fila -1 Columna, indicesHex2=-1Fila +1 Columna 
+    # 6.-Para evitar indices fueras de rango, convertiremos las coordenadas a la matriz del Mal extendida, y ahi se buscaran el hex cifrado
+    # 7.-Retorna el valor de la matriz(hex) que apunta los indices operados  
+    index=0
+    MessageHexList=ASCIItoHEX(mensaje)
+    MatrizExt=HexMatrizExt(matriz)
+    textoCifrado=""
+    print(matriz)
+    print('\nIndices : ', indices)
+    print('\nMensajeHex : ', MessageHexList ,'\n')
+    print('\nMatrix extendida:', MatrizExt, '\n')
+    for OneParHex in range(int(len(MessageHexList)/2)):
+        #FilaColumna -> FC
+        FCFirstHex=indices[stringHexToHex(MessageHexList[index])]
+        FCSecondHex=indices[stringHexToHex(MessageHexList[index+1])]
+        desp=desplazamiento(FCFirstHex, FCSecondHex)
+        print('Par Hex mensaje :[', MessageHexList[index], MessageHexList[index+1], '] Indices HexMensajes: ', FCFirstHex,FCSecondHex,  ' => Desplazamiento: ', desp)
+        
+        index+=2
+        if(desp=="D"):
+            #Operamos los indices de forma Diagonal
+            FCFirstHexCifrado=[FCFirstHex[0]-1,FCFirstHex[1]-1]
+            FCSecondHexCifrado=[FCSecondHex[0]+1,FCSecondHex[1]+1]
+        elif(desp=="V"):
+            FCFirstHexCifrado=[FCFirstHex[0]-1,FCFirstHex[1]]
+            FCSecondHexCifrado=[FCSecondHex[0]+1,FCSecondHex[1]]
+        elif(desp=="H"):
+            FCFirstHexCifrado=[FCFirstHex[0],FCFirstHex[1]-1]
+            FCSecondHexCifrado=[FCSecondHex[0],FCSecondHex[1]+1]
+
+        #Conversion coordenadas del MAL a MALextendida
+        FCFirstHexCifrado=[FCFirstHexCifrado[0]+1, FCFirstHexCifrado[1]+1]
+        FCSecondHexCifrado=[FCSecondHexCifrado[0]+1, FCSecondHexCifrado[1]+1]
+
+        #Obtemos los hex encriptados y los agregamos al mensaje cifrado
+        textoCifrado+=MatrizExt[FCFirstHexCifrado[0]][FCFirstHexCifrado[1]]+MatrizExt[FCSecondHexCifrado[0]][FCSecondHexCifrado[1]]+" "
+
+        print( 'IndicesDesplazados y en Extendida: ',FCFirstHexCifrado, FCSecondHexCifrado, 'Hex Cifrado: ',textoCifrado,'\n' )
+    return textoCifrado
+
+def HexMatrizExt(matriz):
+    matrizExt=[["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""]]
+    #LLenando las esquinas de los bordes
+    matrizExt[0][0]=matriz[3][3]
+    matrizExt[0][5]=matriz[3][0]
+    matrizExt[5][0]=matriz[0][3]
+    matrizExt[5][5]=matriz[0][0]
+    #Llenando el resto de bordes
+    for b1 in range(4):
+        matrizExt[0][b1+1]=matriz[3][b1]
+        matrizExt[b1+1][0]=matriz[b1][3]
+        matrizExt[5][b1+1]=matriz[0][b1]
+        matrizExt[b1+1][5]=matriz[b1][0]
+
+    #LLenando el resto con la matriz del MAL
+    for i in range(4):  
+        for j in range(4):
+            matrizExt[i+1][j+1]=matriz[i][j]
+    
+    return matrizExt
+
 if __name__ == "__main__":
-    indices=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-    indices1=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-    print(hexToASCII("636c617665"))
-    print(ASCIItoHEX("clave"))
-    hex = '636c617665'
-    llave=[""]
-    llave.clear()
-    for letter in hex:
-        llave.append(letter)
-    print(len(llave))
-    print(ConstruirMatriz("636c617665",indices))
-    print(ConstruirMatriz("631c5e4729f",indices1))
+
+    file1 = open('./LlaveMensajeDelMal.txt')
+    LlaveDelMal = file1.readline()
+    MensajeDelMal = file1.readline()
+
+    indices2=[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+    print(CifrarMensaje(ConstruirMatriz(ASCIItoHEX(LlaveDelMal),indices2), indices2,MensajeDelMal))
